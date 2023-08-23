27c9dad71218eca262f72d63d94bf6dbbc9ed3c3
hiralalhere <142942214+hiralalhere@users.noreply.github.com>
2023-08-23 12:34:57 +0300

    Update streamlit_app.py
    Update 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
-('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 
-'Muri n Chanachur'
+streamlit.text('Muri n Chanachur')
+streamlit.text('Plus Tea')
